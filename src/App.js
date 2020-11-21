3d96f75533792cc6deeae6202f01ea0389d76d6d
alinOrwls <71549673+alinOrwls@users.noreply.github.com>
2020-11-21 17:58:53 +0000

    added Github URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+       This project is created by Alin Ortiz and is {"  "}
+     <a href="https://github.com/alinOrwls/weather-alin" target="_blank">open-source on Github</a>
+     
+     </footer>
+     
  
     </div>
   );
