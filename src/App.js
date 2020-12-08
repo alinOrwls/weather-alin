d48971f332a202332023d5bc07995aafe6725a16
alinOrwls <71549673+alinOrwls@users.noreply.github.com>
2020-12-08 18:23:57 +0000

    change c for a C capital

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <Weather defaultcity="Tokyo"/>
+       <Weather defaultCity="Tokyo"/>
        <footer>
        This project is created by Alin Ortiz and is {"  "}
        <a href="https://github.com/alinOrwls/weather-alin" target="_blank">open-source on Github</a>
